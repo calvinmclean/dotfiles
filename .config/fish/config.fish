670520f40f21e584c9b95707c908c6b197e1beb8
Calvin McLean <calvinlmc@gmail.com>
2024-03-06 05:14:24 +0000

    Remove omf config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,2 @@
 alias t='task'
 alias g='git'
-
-if test -d (brew --prefix)"/share/fish/completions"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-end
-
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-end
