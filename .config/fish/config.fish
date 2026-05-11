d24a86642970c6b9b0ec7336f6a6c6c2c9e3715d
Calvin McLean <calvin@obot.ai>
2026-05-11 09:46:38 -0700

    Improve lwt task with fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,13 @@
 alias t='task'
 alias g='git'
+alias zwt='z (t lwt)'
 
 set --export EDITOR vim
-
 set --export --prepend PATH "/Users/calvinmclean/go/bin"
 set --export --prepend PATH "/opt/homebrew/bin"
+
+fzf --fish | source
+
+zoxide init fish | source
+
+fish_vi_key_bindings
